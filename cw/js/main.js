b2aeda87c0179df79a7e7bcd663938eaa3b2a31d
Sabokhat Kalandarova <sabohat2802@gmail.com>
2020-12-09 15:36:30 +0500

    update: nav toggle js with comments

--- cw/js/main.js
+++ cw/js/main.js
@@ -1,7 +1,8 @@
+//Navigation toggle
 var navToggle = document.getElementsByClassName("nav-toggle")[0];
 var aside = document.getElementsByClassName("aside")[0];
 
 navToggle.addEventListener("click", function(){
     aside.classList.toggle("open");
-    console.log("I am listening ")
+    //when toggle is clicked it opens/closed aside
 })
